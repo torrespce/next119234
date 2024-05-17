c7de1e436a095f118949ebd6ae754a689010758d
torrespce <148746919+torrespce@users.noreply.github.com>
2024-05-17 17:01:10 +0200

    edit2

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Test() {
+export default function New() {
     return (
         <div className="h-screen flex items-center justify-center">
             <div className="text-center">
-                <p className="text-5xl text-orange-500 font-bold">Druga strona</p>
-                <Link href="/">Strona główna</Link>
+                <p className="text-5xl text-orange-500 font-bold">WITAJ NA STRONIE 2</p>
                 <div className="py-8">
-                    <Image src="/123.jpg" alt="lodz" width={600} height={400} />
+                    <Image src="/123.jpg" alt="lodz" width={600} height={400}/>
                 </div>
+                <p></p>
+                <Link href="/">Strona główna</Link>
+
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ export default function Home() {
   return (
       <div className="h-screen flex items-center justify-center">
         <div className="text-center">
-          <p className="text-5xl text-red-600 font-bold tracking-tighter">
-            Łukasz Chęciński
-          </p>
-          <p className="text-2xl">Nr albumu: 119234</p>
+          <h1 className="text-blue-800xl text-blue-800">Łukasz Chęciński</h1>
+          <p className="text-5xl">Nr albumu: 119234</p>
+
           <Link href="/new">PAGE 2</Link>
         </div>
       </div>
